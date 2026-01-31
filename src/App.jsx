182821d7a5910314a3bc6d3ecf9fd017fb6c6506
Beca <234265987+becsnr@users.noreply.github.com>
2026-01-31 10:09:56 -0300

    aula06

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import './App.css'
-import Aula05 from './aulas/Aula05'
+import Aula06 from './aulas/Aula06'
 // import Ex005 from './exercicios/ex005/Ex005'
 
 function App() {
   return (
-    <Aula05 />
+    <Aula06 />
     // <Ex005 />
   )
 }
