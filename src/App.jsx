8658f909d30eddc0643d668a3113e93678ad7ec8
Beca <234265987+becsnr@users.noreply.github.com>
2026-02-02 14:26:03 -0300

    onClick + props

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import './App.css'
-import Aula06 from './aulas/Aula06'
+import Aula07 from './aulas/Aula07'
 // import Ex005 from './exercicios/ex005/Ex005'
 
 function App() {
   return (
-    <Aula06 />
+    <Aula07 />
     // <Ex005 />
   )
 }
---
+++ src/aulas/Aula07.jsx
@@ -0,0 +1,14 @@
+import Evento from "../components/Evento";
+
+function Aula07() {
+    return (
+        <>
+            <h1>Testanto eventos</h1>
+            <Evento />
+            <Evento numero="2" />
+        </>
+        
+    )
+}
+
+export default Aula07;
